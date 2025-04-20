7d8d3bc30676aee1c8fd8c242dfe69810363845a
Espi <daniel.alonsoherranz@gmail.com>
2025-04-20 17:04:52 +0200

    carga de eventos en python

---
+++ Análisis/loader.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+
+class Loader:
+
+
+
+    def __init__(self, filepath, loadMultipleFiles = false):
+
+        self.loadFiles(filepath, loadMultipleFiles)
+
+
+    def loadFiles(self, filepath, loadMultipleFiles):
+        self.data = pd.DataFrame()
+        if (loadMultipleFiles):
+            for file in os.listdir(filepath):
+                if file.endswith('.json'):
+                    file_path = os.path.join(filepath, file)
+                    self.data = pd.concat([self.data, pd.read_json(file_path)], ignore_index=True)
+            pass
+        else:
+            self.data = pd.read_json(filepath)
+            pass
