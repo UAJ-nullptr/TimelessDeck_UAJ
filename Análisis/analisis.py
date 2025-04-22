e1358a98e656eee1dd66b55289d44ef958051b28
Rafael Argandoña Blacido <rargan01@ucm.es>
2025-04-23 00:20:09 +0200

    Update del analisis
    Falta: return de datos y catch de datos

--- Análisis/analisis.py
+++ Análisis/analisis.py
@@ -6,10 +6,25 @@ from loader import Loader
 def levelValues(levelDF, event_type_map):
     counts = []
     for code in event_type_map.keys():
-        count = len(levelDF[levelDF["eType"] == code]) # Cambiar por eType cuando haya telemetria
+        count = len(levelDF[levelDF["eType"] == code]) 
         counts.append(count)
 
     return np.array(counts)
+
+def calculateOvertime(levelDF, level_end_event):
+    if (level_end_event['win'] == "false"):
+        return -1
+    
+    event = levelDF[levelDF["eType"] == "LEVEL_EXIT_POSSIBLE"]
+    return level_end_event['timeStamp'] - event['timeStamp']
+
+def calculateEffectiveHeal(levelDF):
+    healing_events = levelDF[levelDF["eType"] == "PLAYER_HEALED"]
+    return np.mean(((healing_events['lifeAfterHeal'] - healing_events['lifeBeforeHeal']) / healing_events['attemptedHeal']).to_numpy())
+
+def calculateMana(levelDF):
+    mana_events = levelDF[levelDF["eType"] == "MANA_TAKEN"]
+    return np.mean((mana_events['manaAfter'] - mana_events['manaBefore']).to_numpy())
 
 def meanSession(sessionDF, event_types):
     level_start_event = sessionDF[sessionDF["eType"] == "START_LEVEL"]
@@ -18,39 +33,50 @@ def meanSession(sessionDF, event_types):
     if len(level_start_event) != len(level_end_event):
         raise ValueError("Mismatch between START_LEVEL and END_LEVEL events")
     event_counters = []
-
-    # Sera reemplazado por
-    # level_start_event = sessionDF[sessionDF["eType"] == "START_LEVEL"]
-    # level_end_event = sessionDF[sessionDF["eType"] == "END_LEVEL"]
+    level_overtime = []
+    effective_healing = []
+    effective_mana_taken = []
 
     for i in range(len(level_start_event)):
+        # Aislamos la parte del dataframe del nivel i
         level_start_time = level_start_event['timeStamp'].iloc[i]
         level_end_time = level_end_event['timeStamp'].iloc[i]
         levelDF = sessionDF[(sessionDF["timeStamp"] > level_start_time) & (sessionDF["timeStamp"] < level_end_time)]
+        # Media de eventos
         event_counters.append(levelValues(levelDF, event_types))
-    return np.mean(np.array(event_counters), axis=0)
+        # Media de curacion efectiva y mana recogido
+        effective_healing.append(calculateEffectiveHeal(levelDF))
+        effective_mana_taken.append(calculateMana(levelDF))
+        # Media de tiempo entre END_LEVEL - LEVEL_EXIT_POSSIBLE
+        overtime = calculateOvertime(levelDF, level_end_event.iloc[i])
+        if (overtime >= 0):
+            level_overtime.append(overtime)
 
-def calculateMean(df_sorted, event_types):
-    #game_start_events = df_sorted[df_sorted["eType"] == "GAME:START"]
-    #game_end_events = df_sorted[df_sorted["eType"] == "GAME:END"]
+    return np.mean(np.array(event_counters), axis=0), np.mean(np.array(level_overtime), axis=0)
 
-    # Sera reemplazado por
+def calculateAnalitics(df_sorted, event_types, inventory_types):
     game_start_events = df_sorted[df_sorted["eType"] == "START_SESSION"]
     game_end_events = df_sorted[df_sorted["eType"] == "END_SESSION"]
-    events_counters = []
+    events_means = []
+    inventory_means = []
 
     for i in range(len(game_start_events)):
+        # Aislamos la parte del dataframe de la sesion i
         session_start_time = game_start_events['timeStamp'].iloc[i]
         session_end_time = game_end_events['timeStamp'].iloc[i]
         sessionDF = df_sorted[(df_sorted["timeStamp"] > session_start_time) & (df_sorted["timeStamp"] < session_end_time)]
-        events_counters.append(meanSession(sessionDF, event_types))
-    return np.mean(np.array(events_counters), axis=0)
+        # Medias de los eventos en nivel - Partida
+        events_means.append(meanSession(sessionDF, event_types))
+        # Medias de los eventos en sesion - Inventario
+        inventory_means.append(levelValues(sessionDF, inventory_types))
+
+    return np.mean(np.array(events_means), axis=0)
 
 def convertValues(df_sorted):
     data_converted = df_sorted.copy()
     data_converted['eType'] = data_converted['eType'].map({0: 'START_SESSION', 1:'END_SESSION', 2:'START_LEVEL', 3:'END_LEVEL', 4:'CARD_CHANGED',
-                                                 5:'ABILITY_USED', 6:'PLAYER_HEALED', 7:'PERIODIC_HEALTH_STATUS', 8:'MANA_TAKEN', 9:'NOT_ENOUGHT_MANA',
-                                                 10:'LEVEL_EXIT_POSSIBLE', 11:'TRIED_LEAVING', 12:'MOVED_FROM_HAND', 13:'MOVED_TO_HAND', 14:'INVENTORY_LEFT'})
+                                                 5:'ABILITY_USED', 6:'PLAYER_HEALED', 7:'MANA_TAKEN', 8:'NOT_ENOUGHT_MANA', 9:'LEVEL_EXIT_POSSIBLE', 
+                                                 10:'LEAVE_FAILED', 11:'MOVED_FROM_HAND', 12:'MOVED_TO_HAND', 13:'INVENTORY_LEFT'})
     return data_converted
 
 loader = Loader("TimlessDeck-12036-1745274911802608300.json", False)
@@ -61,13 +87,15 @@ dataSorted = dataSorted.sort_values("timeStamp")
 # Display all rows of the DataFrame
 pd.set_option("display.max_rows", None)
 
-event_type_map = { 4: 'CARD_CHANGED', 5: 'ABILITY_USED', 6: 'PLAYER_HEALED', 7: 'PERIODIC_HEALTH_STATUS',
-    8: 'MANA_TAKEN', 9: 'NOT_ENOUGHT_MANA', 10: 'LEVEL_EXIT_POSSIBLE',
-    11: 'TRIED_LEAVING', 12: 'MOVED_FROM_HAND', 13: 'MOVED_TO_HAND', 14: 'INVENTORY_LEFT'
+event_type_map = { 4:'CARD_CHANGED', 5:'ABILITY_USED', 
+                  8:'NOT_ENOUGHT_MANA', 9:'LEVEL_EXIT_POSSIBLE', 
+                  10:'LEAVE_FAILED'
 }
+
+inventory_type_map = { 11:'MOVED_FROM_HAND', 12:'MOVED_TO_HAND'}
 
 # Sacar la media
 dataSorted = convertValues(dataSorted)
-means = calculateMean(dataSorted, event_type_map) # Se reemplazara por event_type_map
+means = calculateAnalitics(dataSorted, event_type_map, inventory_type_map) # Se reemplazara por event_type_map
 print(means)
 # display(means)
